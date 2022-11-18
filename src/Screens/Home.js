722d761e814357cdfe36aadaac58cd51150a4753
MarcotulliMatteo <marcotullimatteo5b@gmail.com>
2022-11-18 14:26:14 +0100

    feat: Styled Component encapsulation

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,6 +1,6 @@
 import MapCanvas from "../components/MapCanvas";
 import React from "react";
-import styled from "styled-components";
+import { HomeBody, ContainerComponent, MapContainer } from '../StyledComponents/StyledComponentHome';
 import FlightList from "../components/FlightList";
 
 const Home = () => {
@@ -19,25 +19,3 @@ const Home = () => {
 }
 
 export default Home;
-
-const HomeBody = styled.div`
-    background-color: lightgray;
-    padding: 0px 20px 0px 20px;
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-`
-
-const ContainerComponent = styled.div`
-    border-radius: 10px;
-    background-color: white;
-    display: flex;
-    flex-direction: row;
-`
-
-const MapContainer = styled.div`
-    justify-content: center;
-    display: flex;
-`
---
+++ src/StyledComponents/StyledComponentHome.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const HomeBody = styled.div`
+    background-color: lightgray;
+    padding: 0px 20px 0px 20px;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+`
+
+export const ContainerComponent = styled.div`
+    border-radius: 10px;
+    background-color: white;
+    display: flex;
+    flex-direction: row;
+`
+
+export const MapContainer = styled.div`
+    justify-content: center;
+    display: flex;
+`
